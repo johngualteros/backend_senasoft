2cfff8d7fc05124cab5cd62e190a9277d0d6a428
john <john@gmail.com>
2022-09-06 15:00:18 -0500

    first commit

---
+++ src/test/java/com/senasoft/sena/SenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senasoft.sena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
